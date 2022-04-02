48f32c7017507d2ebafba3f20d30e9adeb9d2b24
sani kumar <sanikumar2121@gmail.com>
2022-04-02 08:52:52 +0530

    Update headerh1.h

--- headerh1.h
+++ headerh1.h
@@ -1,1 +1,19 @@
+#ifndef h1;
+#define h1;
+if unique varible 
+   iostream.h
+   
+   
+   else if 
+     algorithm.h
+     #ifndef stack
+     #define stack
+     int stack1_empty()
+   {
+     return 0
+   } 
 
+     
+
+
+end if
