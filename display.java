360e7bcb99ae1107f7ac14b7a74a5092cccb54e3
sani kumar <sanikumar2121@gmail.com>
2022-10-09 13:34:58 +0530

    Create display.java

---
+++ display.java
@@ -0,0 +1,16 @@
+import java.util.*;
+ 
+ 
+ class display
+
+ {
+    public static void main (String[] args)
+   {
+       System.out.println("underlove");
+       Scanner sc= new Scanner(System.in );
+       int a = sc.nextInt();
+              System.out.println(a);
+
+       sc.close();
+   }
+ }
