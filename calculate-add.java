5dc3b9dd76b5d5d7bc712410e0d98e79e0cfa079
sani kumar <sanikumar2121@gmail.com>
2022-10-12 12:03:23 +0530

    Create calculate-add.java

---
+++ calculate-add.java
@@ -0,0 +1,17 @@
+import java.io;
+import java.ntil.*;
+
+class add
+{
+  public static void main(String[] args)
+  {
+    Scanner sc = new Scanner(System.in)
+    int a=nextInt();
+  int b=nextInt();
+  int c=a+b;
+  system.out.println(a);
+  sc.close();
+    {
+      {
+        
+  
